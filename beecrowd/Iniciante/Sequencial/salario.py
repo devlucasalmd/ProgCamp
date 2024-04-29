c83ccdf4694c00b5de126ca83007fe883333475c
devlucasalmd <lucas.amaral.almd@gmail.com>
2024-04-29 15:18:05 -0300

    feat: adicionando exercicios em python da fase Iniciante na beecrowd

---
+++ beecrowd/Iniciante/Sequencial/salario.py
@@ -0,0 +1,7 @@
+num = int(input())
+hr = int(input())
+valor = float(input())
+
+sal = valor * hr
+print("NUMBER =", num)
+print("SALARY = U$ {:.2f}".format(sal))
---
+++ beecrowd/Iniciante/Sequencial/salariobonus.py
@@ -0,0 +1,9 @@
+nome = str(input())
+sal = float(input())
+vendas = float(input())
+
+total = vendas * 0.15 + sal
+
+print('TOTAL = R$ {:.2f}'.format(total))
+
+
